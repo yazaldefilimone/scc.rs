465cd21815b0d0a231e7867f889305b5163d44b5
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-06-21 13:35:45 +0200

    *

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+mod ast;
+mod format;
+mod parser;
+use parser::MdxParser;
+
 fn main() {
-    println!("Hello, world!");
+  let mdx = r#"
+# Hello World
+
+This is a test.
+
+## Hello World
+
+This is a test.
+
+### Hello World
+
+This is a test.
+
+#### Hello World
+
+This is a test.
+
+##### Hello World
+
+This is a test.
+
+###### Hello World
+
+This is a test.
+  "#;
+  let ast = MdxParser::parse(mdx);
+  println!("{:#?}", ast);
 }
