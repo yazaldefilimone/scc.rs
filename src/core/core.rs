9efa4fec2e98ea36eea2d1701e5e9095736d7a49
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-06-22 00:47:20 +0200

    *

--- src/core/core.rs
+++ src/core/core.rs
@@ -1,9 +1,26 @@
-pub struct Processor {
+use crate::transformer::Transformer;
+
+pub struct Core {
   pub ast: Vec<String>,
+  pub transformers: Vec<Box<dyn Transformer>>,
 }
 
-impl CoreProcessor {
+impl Core {
   pub fn new() -> Self {
-    Self { ast: vec![] }
+    Self { ast: vec![], transformers: vec![] }
+  }
+  pub fn run(&self) {
+    self.process_transformers();
+    println!("{:#?}", self.ast);
+  }
+
+  pub fn use_transformer(&mut self, transformer: Box<dyn Transformer>) {
+    self.transformers.push(transformer);
+  }
+
+  pub fn process_transformers(&mut self) {
+    for transformer in &self.transformers {
+      transformer.transform(&mut self.ast);
+    }
   }
 }
