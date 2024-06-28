4fe4bb81abac568c96e739ceeccba35a37380a17
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-06-28 17:44:42 +0200

    *

--- src/core/core.rs
+++ src/core/core.rs
@@ -1,26 +1,42 @@
-use crate::transformer::Transformer;
+use crate::{ast, parser::MdxParser, transformer::Transformer};
 
-pub struct Core {
-  pub ast: Vec<String>,
-  pub transformers: Vec<Box<dyn Transformer>>,
+pub struct Core<'a> {
+  ast: Option<ast::Ast>,
+  transformers: Vec<Box<dyn Transformer>>,
+  raw: &'a str,
 }
 
-impl Core {
+impl<'a> Default for Core<'a> {
+  fn default() -> Self {
+    Self::new()
+  }
+}
+
+impl<'a> Core<'a> {
   pub fn new() -> Self {
-    Self { ast: vec![], transformers: vec![] }
-  }
-  pub fn run(&self) {
-    self.process_transformers();
-    println!("{:#?}", self.ast);
+    Self { transformers: vec![], raw: "", ast: None }
   }
 
-  pub fn use_transformer(&mut self, transformer: Box<dyn Transformer>) {
+  pub fn use_transformer(mut self, transformer: Box<dyn Transformer>) -> Self {
     self.transformers.push(transformer);
+    self
   }
 
-  pub fn process_transformers(&mut self) {
-    for transformer in &self.transformers {
-      transformer.transform(&mut self.ast);
+  fn transform(&mut self) {
+    if let Some(ast) = &mut self.ast {
+      for transformer in &mut self.transformers {
+        let mut ast = ast.clone();
+        self.ast = Some(transformer.transform(&mut ast).clone());
+      }
     }
+  }
+
+  pub fn compile(&mut self, raw: &str) {
+    self.ast = Some(self.parse(raw));
+    self.transform();
+  }
+
+  pub fn parse(&self, raw: &str) -> ast::Ast {
+    return MdxParser::parse(raw);
   }
 }
